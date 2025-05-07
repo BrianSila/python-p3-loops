a8948806e1dee933bf63e0f7a4100cb40b778f1a
Brian <brian.sila1@student.moringaschool.com>
2025-05-07 15:32:52 +0300

    fizzbuzz function fixed

--- lib/looping.py
+++ lib/looping.py
@@ -8,11 +8,11 @@ def happy_new_year():
     print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
+    
 
-def fizzbuzz(num):
-    for num in range(1, 100):
+def fizzbuzz():
+    for num in range(1, 101):
         if num % 3 == 0 and num % 5 == 0:
             print("FizzBuzz")
         elif num % 3 == 0:
@@ -21,3 +21,4 @@ def fizzbuzz(num):
             print("Buzz")
         else:
             print(num)
+            
